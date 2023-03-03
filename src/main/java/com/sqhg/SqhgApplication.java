ad931e376d6bc09d42e8c67f2e56e07ccdc54a97
Paulo Vitor Schio <paulovschio@gmail.com>
2023-03-03 20:53:16 -0300

    First commit: added Spring Boot project

---
+++ src/main/java/com/sqhg/SqhgApplication.java
@@ -0,0 +1,13 @@
+package com.sqhg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqhgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqhgApplication.class, args);
+	}
+
+}
