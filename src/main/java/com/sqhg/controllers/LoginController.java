0135f0a0c20d46defaafb1bc652cd0380c96a5e8
MateusBPK <mateusbpk@gmail.com>
2023-03-20 15:42:59 -0300

    correção

---
+++ src/main/java/com/sqhg/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.sqhg.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class LoginController {
+
+    @GetMapping(value = "/")
+    public String login() {
+        return "login";
+    }
+}
--- src/main/java/com/sqhg/repositories/AdministradorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sqhg.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sqhg.entities.Administrador;
-
-public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-    
-}
