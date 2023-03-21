c85aed75cd8c05249768dc304663a683881eee68
MateusBPK <mateusbpk@gmail.com>
2023-03-20 22:11:11 -0300

    Front
    Co-Authored-By: SFreiress <107963350+SFreiress@users.noreply.github.com>

--- src/main/java/com/sqhg/controllers/LoginController.java
+++ src/main/java/com/sqhg/controllers/LoginController.java
@@ -2,15 +2,15 @@ package com.sqhg.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping
+
 public class LoginController {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/login")
     public String login() {
         return "login";
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/resources/templates/index.html"],
+  content: ["./src/main/resources/templates/login.html"],
   theme: {
     extend: {},
   },
