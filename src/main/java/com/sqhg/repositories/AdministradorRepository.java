ce5c68533e513f795ead0bccf6714028b8f9a416
MateusBPK <mateusbpk@gmail.com>
2023-03-22 17:46:41 -0300

    Update AdministradorRepository.java

--- src/main/java/com/sqhg/repositories/AdministradorRepository.java
+++ src/main/java/com/sqhg/repositories/AdministradorRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sqhg.entities.Administrador;
 
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-    
+    @Query(value="select * from administrador where email = :email and senha = :senha", nativeQuery = true);
+
+        
 }
