345c741ffd46743fca797a3e0c227b3926ac2ea3
Matheus Soares Gomes <matheus.soares@msn.com>
2023-05-05 20:01:50 -0300

    WIP: front home
    WIP: front home

---
+++ src/main/java/com/sqhg/controllers/LoginController.java
@@ -0,0 +1,35 @@
+package com.sqhg.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sqhg.entities.Administrador;
+import com.sqhg.repositories.AdministradorRepository;
+
+@Controller
+
+public class LoginController {
+    @Autowired
+    private AdministradorRepository repository;
+
+    @Autowired
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping(value = "/logar")
+    public String logar(Model model, Administrador admParam) {
+        Administrador adm = this.repository.Login(admParam.getCracha(),
+                admParam.getSenha());
+        if (adm != null) {
+            return "";
+        }
+        model.addAttribute("erro", "Usuário ou senha inválidos");
+        return "login";
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+-- INSERT INTO tb_department(name) VALUES ('RH');
+-- INSERT INTO tb_department(name) VALUES ('TI');
+
+-- INSERT INTO tb_user(department_id, email, senha) VALUES (1, 'teste', '123');
+-- INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
+-- INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
+-- INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
+-- INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
+INSERT INTO administrador(id, cracha, email, nome, senha, telefone) 
+                  VALUES (1, '123','teste1@gmail.com', 'teste1', '123', '99999 9999');
+INSERT INTO administrador(id, cracha, email, nome, senha, telefone) 
+                  VALUES (2, '123654','teste2@gmail.com', 'teste2', '1234', '8888 9999');
