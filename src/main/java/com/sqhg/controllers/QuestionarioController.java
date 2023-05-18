101a558199b4c4ccc6070dfa1dc352c13ba7b0dd
Geovana Hekce <geovana.shecke@gmail.com>
2023-05-18 20:23:20 -0300

    WIP tela de envio de questionário 18/05

---
+++ src/main/java/com/sqhg/controllers/QuestionarioController.java
@@ -0,0 +1,20 @@
+package com.sqhg.controllers;
+
+import com.sqhg.forms.AdministradorForm;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/questionario")
+@AllArgsConstructor
+public class QuestionarioController {
+
+    @GetMapping(value="/novo")
+    public String enviarQuestionario(Model model) {
+        model.addAttribute("questionario", new AdministradorForm());
+        return "envioQuestionario";
+    }
+}
