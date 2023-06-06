8d3a91c7a79e4ff590c604b38a49be3bf567995e
Mateus Vinicius Miotto <mateusbpk@gmail.com>
2023-06-06 09:05:36 -0300

    Fix Login page layout

--- src/main/java/com/sqhg/controllers/ResponderQuestionarioController.java
+++ src/main/java/com/sqhg/controllers/ResponderQuestionarioController.java
@@ -4,45 +4,42 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.sqhg.entities.Administrador;
 import com.sqhg.entities.Questao;
 import com.sqhg.entities.Questionario;
-import com.sqhg.repositories.QuestionarioRepository;
 import com.sqhg.services.QuestaoService;
 import com.sqhg.services.QuestionarioService;
 
-@RestController
+import lombok.AllArgsConstructor;
+
+@Controller
 @RequestMapping("/questionario")
+@AllArgsConstructor
 public class ResponderQuestionarioController {
 
     private QuestionarioService questionarioService;
     private QuestaoService questaoService;
 
-    @Autowired
-    public ResponderQuestionarioController(QuestionarioService questionarioService, QuestaoService questaoService) {
-        this.questionarioService = questionarioService;
-        this.questaoService = questaoService;
-    }
-
     @GetMapping("/{modeloquestionarioId}")
-    public List<Questao> encontrarQuestoesPorModeloQuestionario(
+    public String encontrarQuestoesPorModeloQuestionario(Model model,
             @PathVariable(name = "modeloquestionarioId") long modeloquestionarioId) {
 
         Questionario questionario = questionarioService.findById(modeloquestionarioId);
         Administrador administrador = questionario.getAdministrador();
+
         System.out.println(administrador.getNome());
 
+        model.addAttribute("administrador", administrador.getNome());
         List<Questao> ListaQuestoesModeloQuestionarioQuestionario = questaoService
                 .encontrarQuestoesPorModeloQuestionario(modeloquestionarioId).stream()
                 .collect(Collectors.toList());
 
-        return ListaQuestoesModeloQuestionarioQuestionario;
+        return "responderQuestaoEl";
     }
 }
