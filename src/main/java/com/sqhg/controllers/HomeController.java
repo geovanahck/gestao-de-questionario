b8b219ea3271fbed14f321bac7ce4208fa777409
Mateus Vinicius Miotto <mateusbpk@gmail.com>
2023-06-16 17:37:03 -0300

    Commit

--- src/main/java/com/sqhg/controllers/HomeController.java
+++ src/main/java/com/sqhg/controllers/HomeController.java
@@ -3,16 +3,11 @@ package com.sqhg.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class HomeController {
 
-    @GetMapping("/cadadministrador")
-    public String cadadministrador() { return "cadAdministrador";  }
-
-    @GetMapping("/listaadm")
-    public String listadm() { return "listaAdm";  }
-
-
-
+    @GetMapping("/home")
+    public String cadadministrador() {
+        return "home";
+    }
 }
