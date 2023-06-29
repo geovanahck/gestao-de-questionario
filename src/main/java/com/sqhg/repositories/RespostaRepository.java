82ef554c5bcf22adbc9e60b026e502db9db1cdc5
Paulo Vitor Schio <paulovschio@gmail.com>
2023-06-29 20:38:51 -0300

    Added Resposta Repository

---
+++ src/main/java/com/sqhg/repositories/RespostaRepository.java
@@ -0,0 +1,9 @@
+package com.sqhg.repositories;
+
+import com.sqhg.entities.Resposta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RespostaRepository extends JpaRepository<Resposta, Long> {
+}
