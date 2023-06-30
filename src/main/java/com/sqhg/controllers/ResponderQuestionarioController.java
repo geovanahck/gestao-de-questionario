0feeb1b0d033c9affee21186b46ccd9a2c586e73
Paulo Vitor Schio <paulovschio@gmail.com>
2023-06-30 18:40:46 -0300

    tela responder

--- src/main/java/com/sqhg/controllers/ResponderQuestionarioController.java
+++ src/main/java/com/sqhg/controllers/ResponderQuestionarioController.java
@@ -3,40 +3,63 @@ package com.sqhg.controllers;
 import com.sqhg.entities.Questao;
 import com.sqhg.entities.Questionario;
 import com.sqhg.entities.Resposta;
+import com.sqhg.entities.TipoQuestao;
 import com.sqhg.services.QuestionarioService;
 import com.sqhg.services.RespostaService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/questionarios/responder/{codigoQuestionario}")
+@RequestMapping("/questionario/responder/{codigoQuestionario}")
 @AllArgsConstructor
 public class ResponderQuestionarioController {
 
     private final QuestionarioService questionarioService;
     private final RespostaService respostaService;
 
-    @PostMapping("/{numeroQuestao}")
-    public String salvarQuestaoeIrParaProximaQuestao(Model model,
-                                                     @PathVariable(name = "codigoQuestionario") Integer codigoQuestionario,
-                                                     @PathVariable(name = "numeroQuestao") int numeroQuestao,
-                                                     @ModelAttribute Resposta resposta) {
-
+    @GetMapping("/{numeroQuestao}")
+    public String telaQuestao(Model model,
+                              @PathVariable(name = "codigoQuestionario") String codigoQuestionario,
+                              @PathVariable(name = "numeroQuestao") int numeroQuestao) {
         Questionario questionario = questionarioService.acharQuestionarioPorCodigo(codigoQuestionario);
         if (questionario == null) {
-            return "redirect:/";
+            return "redirect:/login-usuario";
         }
 
-        Questao questaoAtual = questionario.getModeloQuestionario().getQuestoes().get(numeroQuestao);
-        resposta.setQuestionario(questionario);
-        resposta.setQuestao(questaoAtual);
-        respostaService.salvarResposta(resposta);
+        Questao questaoAtual = questionario.getModeloQuestionario().getQuestoes().get(numeroQuestao - 1);
+        model.addAttribute("questao", questaoAtual);
+        model.addAttribute("resposta", Resposta.builder().questao(questaoAtual).questionario(questionario).build());
+        model.addAttribute("numeroQuestao", numeroQuestao);
+        model.addAttribute("codigoQuestionario", codigoQuestionario);
+        model.addAttribute("ultimaQuestao", questionario.getModeloQuestionario().getQuestoes().size() == numeroQuestao);
 
-        return "redirect:/questionarios/responder/{%d}/{%d}".formatted(codigoQuestionario, numeroQuestao + 1);
+        if (questaoAtual.getTipo().equals(TipoQuestao.MULTIPLA) || questaoAtual.getTipo().equals(TipoQuestao.ALTERNATIVAS)) {
+            model.addAttribute("alternativas", questaoAtual.getOpcoes());
+            return "responderQuestaoAlternativas";
+        } else if (questaoAtual.getTipo().equals(TipoQuestao.ABERTA)) {
+            return "responderQuestaoAberta";
+        }
+        return "responderQuestaoEL";
+    }
+
+    @PostMapping("/{numeroQuestao}")
+    public String salvarResposta(Model model,
+                                 @PathVariable(name = "numeroQuestao") Integer numeroQuestao,
+                                 @PathVariable(name = "codigoQuestionario") String codigoQuestionario,
+                                 @ModelAttribute Resposta resposta) {
+        Questionario questionario = questionarioService.acharQuestionarioPorCodigo(codigoQuestionario);
+        if (questionario == null) {
+            return "redirect:/login-usuario";
+        }
+        respostaService.salvarResposta(resposta);
+        if (numeroQuestao < questionario.getModeloQuestionario().getQuestoes().size()) {
+            return String.format("redirect:/questionario/responder/%s/%d", codigoQuestionario, ++numeroQuestao);
+        }
+        return "redirect:/questionario/lista";
     }
 }
